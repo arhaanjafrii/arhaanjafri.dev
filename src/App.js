6f5540dc9239cf94846076af6c0a0c979ecc36cb
Arhaan Jafri <arhaan.jafri@gmail.com>
2025-06-25 17:31:29 -0500

    Minor Text fix

--- src/App.js
+++ src/App.js
@@ -91,7 +91,7 @@ function MainPage() {
         Hey, I'm Arhaan. <i className="fa-solid fa-heart heart-icon" style={{ color: '#B197FC' }}></i>
       </h1>
       <div className="desc">
-        <p>I'm a highschooler in Austin, Texas. I'm big into robotics. Check out my projects <Link to="/projects/" className="link slightbold">here</Link>!</p>
+        <p>I'm a high school student based in Austin, Texas with a strong passion for robotics. You can check out my projects <Link to="/projects/" className="link slightbold">here</Link>!</p>
         <p style={{marginTop: '1.3em'}}>I am the founder of <a href="https://cirkit.crazeddd.dev/" target="_blank" rel="noopener noreferrer" className="link slightbold">CirKit</a>, a service that delivers projects to your doorstep, designed by students, for students.</p>
       </div>
       <div className="time-status">
